45f3282090e5aff456493e582975600e3e78b5de
Mariana <marianaoliveira.unilasalle@gmail.com>
2016-04-14 13:37:27 +0000

    programasoma

---
+++ soma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    int num1, num2;
+    int soma;
+    
+    num = 9;
+    num = 10;
+    
+    soma = num1 + num2;
+    
+    printf("\nSoma = %d", soma);
+    
+    return 0;
+}
